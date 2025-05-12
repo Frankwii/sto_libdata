a6a9958cedb727b5416cd971df5fa832c5ee120c
Frankwii <frank.william.hammond@gmail.com>
2025-05-12 12:18:28 +0200

    Project-ified code and added license.

--- src/sto_libdata/__init__.py
+++ src/sto_libdata/__init__.py
@@ -1,5 +1,5 @@
-from libdata.connection.connection_handler import ConnectionHandler 
-from libdata.connection.database_connection import init_engine
-from libdata.dataframe_handling.pushable_dataframe import PushableDF, PushConfig
+from sto_libdata.connection.connection_handler import ConnectionHandler 
+from sto_libdata.connection.database_connection import init_engine
+from sto_libdata.dataframe_handling.pushable_dataframe import PushableDF, PushConfig
 
 __all__ = ["ConnectionHandler", "init_engine", "PushableDF", "PushConfig"]
