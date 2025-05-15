11b6b93cf2737973b0a9512cda7e73b1b656e860
Frankwii <frank.william.hammond@gmail.com>
2025-05-15 15:54:43 +0200

    Coded naming conventions into the type inference system

--- src/sto_libdata/dataframe_handling/dataframe_handler.py
+++ src/sto_libdata/dataframe_handling/dataframe_handler.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 from pandas.io.sql import Series
-from sqlalchemy.types import TypeEngine as SQLType
+from sqlalchemy import DATETIME, Boolean, Date, Float, Integer
+from sqlalchemy.types import String, TypeEngine as SQLType
+
+
+class PotentialCHAR(SQLType):
+    ...
+
+class UnknownType(SQLType):
+    ...
+
 
 class DataFrameHandler:
     def __init__(self) -> None:
@@ -15,7 +24,39 @@ class DataFrameHandler:
 
     def infer_SQL_types(self, df: pd.DataFrame) -> dict[str, SQLType]:
 
-        return {str(col): self.infer_SQL_type(Series(df[col])) for col in df.columns}
+        return {str(col_name): self.infer_SQL_type(Series(df[col_name]), col_name) for col_name in df.columns}
 
-    def infer_SQL_type(self, col: pd.Series | pd.DataFrame) -> SQLType:
-        raise NotImplementedError("Pending")
+    def infer_by_name(self, column_name: str) -> SQLType:
+        match [c for c in column_name.capitalize()]:
+            case ["I", "D", "_", *c]:
+                return Integer()
+            case ["D", "S", "_", *c]:
+                return String() # VARCHAR(MAX)
+            case ["T", "X", "_", *c] | ["C", "O", "_", *c]:
+                return PotentialCHAR()
+            case ["S", "W", "_", *c]:
+                return Boolean()
+            case ["D", "A", "_", *c]:
+                return Date()
+            case ["T", "S", "_", *c]:
+                return DATETIME()
+            case [*c, "_", "E", "U", "R"] | [*c, "_", "U", "S", "D"]:
+                return Float()
+            case [*c, "C", "O", "U", "N", "T"]:
+                return Integer()
+            case _:
+               return UnknownType()
+
+    def infer_by_value(self, col: pd.Series | pd.DataFrame) -> SQLType:
+        return UnknownType()
+
+    def infer_SQL_type(self, col: pd.Series | pd.DataFrame, col_name: str) -> SQLType:
+        type_by_naming_convention= self.infer_by_name(col_name)
+        if not isinstance(type_by_naming_convention, UnknownType | PotentialCHAR):
+            return type_by_naming_convention
+
+        type_by_value= self.infer_by_value(col)
+        if not isinstance(type_by_value, UnknownType):
+            return type_by_value
+
+        raise TypeError(f"Unable to infer type for column {col}")
